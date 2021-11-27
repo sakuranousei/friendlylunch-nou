09af893e8d0be1c66aa2b9f91922d99bd03ff4e9
Glitch (longing-eel) <none>
2021-11-27 12:57:52 +0000

    ❤️🌒 Checkpoint ./public/client.js:38817841/563 ./views/index.ejs:38817841/58

--- public/client.js
+++ public/client.js
@@ -60,3 +60,27 @@ clearButton.addEventListener('click', event => {
     });
   dreamsList.innerHTML = "";
 });
+
+
+//日付
+//今日の日付データを変数に格納
+//変数は"today"とする
+var today=new Date(); 
+//年・月・日・曜日を取得
+var year = today.getFullYear();
+var month = today.getMonth()+1;
+var week = today.getDay();
+var day = today.getDate();
+var week_ja= new Array("日","月","火","水","木","金","土");
+//年・月・日・曜日を書き出す
+document.write(year+"年"+month+"月"+day+"日 "+week_ja[week]+"曜日");
+
+//時刻データを取得して変数に格納する
+//変数は"time"とする
+var time= new Date();
+//時・分・秒を取得
+var hour = time.getHours();
+var minute = time.getMinutes();
+var second = time.getSeconds();
+document.write(hour+"時",+minute+"分"+second+"秒");
+
