30d60d6e9e13f9c8e7ae7a7cfb6ba85df30bb8d6
Glitch (longing-eel) <none>
2021-11-28 07:11:41 +0000

    🏅👭 Checkpoint ./views/index.ejs:38817841/19 ./views/edit.ejs:38817841/19 ./public/client.js:38817841/928

--- public/client.js
+++ public/client.js
@@ -68,23 +68,7 @@ const appendNewUserRadio = user => {
   div.append(label);
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-const getUsers = user => {
-  console.log(user);
-  const parent = document.getElementById("nameFormArea");
-  const tr = document.createElement("tr");
-  const td = document.createElement("td");
-  const input = document.createElement("input");
-    input.className = "form-control";
-    input.type = "text";
-    input.name = "users";
-    input.value = user;
-  parent.appendChild(tr);
-  tr.append(td);
-  td.append(input);
-}
-});
-
+// window.addEventListener('DOMContentLoaded', () => {
 // const getUsers = user => {
 //   console.log(user);
 //   const parent = document.getElementById("nameFormArea");
@@ -99,6 +83,9 @@ const getUsers = user => {
 //   tr.append(td);
 //   td.append(input);
 // }
+// });
+
+
 // function userRowAdd() {
 //   const tr = document.createElement("tr");
 //   const td = document.createElement("td");
@@ -213,4 +200,19 @@ function userDataAdd() {
   for (let i = 0; i < name.length; i++) {
     console.log(name[i].value);
   }
+}
+
+const getUsers = user => {
+  console.log(user);
+  const parent = document.getElementById("nameFormArea");
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+  const input = document.createElement("input");
+    input.className = "form-control";
+    input.type = "text";
+    input.name = "users";
+    input.value = user;
+  parent.appendChild(tr);
+  tr.append(td);
+  td.append(input);
 }
