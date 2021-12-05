db8e3c75f42df32365428a4feda2d686cd4d35b9
Glitch (lace-heliotrope-microceratops) <none>
2021-12-05 12:35:51 +0000

    🏠🎥 Checkpoint ./public/client.js:38817841/1645

--- public/client.js
+++ public/client.js
@@ -44,9 +44,9 @@ fetch("/getMenusData", {})
     response.forEach(row => {
       appendMenuAccordionHeader(row.id, row.store, row.menu, row.price);
     });
-    response.forEach(row => {
-      appendMenuAccordionBody(row.id, row.store, row.menu, row.price);
-    }); 
+    // response.forEach(row => {
+    //   appendMenuAccordionBody(row.id, row.store, row.menu, row.price);
+    // }); 
   });
 
 // fetch("/getMenusData", {})
@@ -176,7 +176,7 @@ const appendMenuAccordionHeader = (id, store, menu, price) => {
     button_store.innerText = store;
   const div2_accordionCollapse = document.createElement("div");
     div2_accordionCollapse.id = `collapse_${id}`;
-    div2_accordionCollapse.className = "accordion-collapse collapse";
+    div2_accordionCollapse.className = `accordion-collapse collapse ${store}`;
     div2_accordionCollapse.setAttribute("aria-labelledby", `heading_${id}`);
   const div3_accordionBody = document.createElement("div");
     div3_accordionBody.className = `accordion-body ${store}`;
@@ -198,13 +198,18 @@ const appendMenuAccordionHeader = (id, store, menu, price) => {
   
   console.log(store);
   console.log(button_store.innerText);
+  const renderedStore = document.getElementsClassName(`${store}`) 
+  console.log(renderedStore);
   
-  if (store == button_store.innerText) {
+  if (store == renderedStore) {
+    renderedStore.append(div3_accordionBody);
+          div3_accordionBody.append(div4_formCheck);
+          div4_formCheck.append(input);
+          div4_formCheck.append(label_menu);
+          div4_formCheck.append(label_price);
     
-  } if (store ==)
-    
-  
-  parent_menuArea.appendChild(div1_accordionItem);
+  } else  {
+    parent_menuArea.appendChild(div1_accordionItem);
     div1_accordionItem.append(h2_accordionHeader);
       h2_accordionHeader.append(button_store);
     div1_accordionItem.append(div2_accordionCollapse);
@@ -213,55 +218,59 @@ const appendMenuAccordionHeader = (id, store, menu, price) => {
           div4_formCheck.append(input);
           div4_formCheck.append(label_menu);
           div4_formCheck.append(label_price);
+  }
+    
+  
+  
 };
 
 //　試し　　indexページ Menusデータ反映 アコーディオン　ボディ
-const appendMenuAccordionBody = (id, store, menu, price) => {
-  const parent = document.getElementById("menusArea");
-  const div_1 = document.createElement("div");
-    div_1.className = "accordion-item";
-  const h2 = document.createElement("h2");
-    h2.className = "accordion-header";
-    h2.id = `heading_${id}`;
-  const button = document.createElement("button");
-    button.className = "accordion-button collapsed";
-    button.type = "button";
-    button.setAttribute("data-bs-toggle", "collapse");
-    button.setAttribute("data-bs-target", `#collapse_${id}`);
-    button.setAttribute("aria-expanded", "false");
-    button.setAttribute("aria-controls", `collapse_${id}`);
-    button.innerText = store;
-  const div_2 = document.createElement("div");
-    div_2.id = `collapse_${id}`;
-    div_2.className = "accordion-collapse collapse";
-    div_2.setAttribute("aria-labelledby", `heading_${id}`);
-  // const body = appendNewMenuAccordionBody(id, store, menu, price);
-  const div_3 = document.createElement("div");
-    div_3.className = `accordion-body ${store}`;
-  const div_4 = document.createElement("div");
-    div_4.className = "form-check";
-  const input = document.createElement("input");
-    input.className = "form-check-input";
-    input.type = "checkbox";
-    input.value = "";
-    input.id = "flexCheckDefault";
-  const label_1 = document.createElement("label");
-    label_1.className = "form-check-label";
-    label_1.for = "flexCheckDefault";
-    label_1.innerText = menu;
-  const label_2 = document.createElement("label");
-    label_2.className = "form-check-label px-3";
-    label_2.for = "flexCheckChecked";
-    label_2.innerText = price;
+// const appendMenuAccordionBody = (id, store, menu, price) => {
+//   const parent = document.getElementById("menusArea");
+//   const div_1 = document.createElement("div");
+//     div_1.className = "accordion-item";
+//   const h2 = document.createElement("h2");
+//     h2.className = "accordion-header";
+//     h2.id = `heading_${id}`;
+//   const button = document.createElement("button");
+//     button.className = "accordion-button collapsed";
+//     button.type = "button";
+//     button.setAttribute("data-bs-toggle", "collapse");
+//     button.setAttribute("data-bs-target", `#collapse_${id}`);
+//     button.setAttribute("aria-expanded", "false");
+//     button.setAttribute("aria-controls", `collapse_${id}`);
+//     button.innerText = store;
+//   const div_2 = document.createElement("div");
+//     div_2.id = `collapse_${id}`;
+//     div_2.className = "accordion-collapse collapse";
+//     div_2.setAttribute("aria-labelledby", `heading_${id}`);
+//   // const body = appendNewMenuAccordionBody(id, store, menu, price);
+//   const div_3 = document.createElement("div");
+//     div_3.className = `accordion-body ${store}`;
+//   const div_4 = document.createElement("div");
+//     div_4.className = "form-check";
+//   const input = document.createElement("input");
+//     input.className = "form-check-input";
+//     input.type = "checkbox";
+//     input.value = "";
+//     input.id = "flexCheckDefault";
+//   const label_1 = document.createElement("label");
+//     label_1.className = "form-check-label";
+//     label_1.for = "flexCheckDefault";
+//     label_1.innerText = menu;
+//   const label_2 = document.createElement("label");
+//     label_2.className = "form-check-label px-3";
+//     label_2.for = "flexCheckChecked";
+//     label_2.innerText = price;
   
-  console.log(store);
-  console.log(button.innerText);
+//   console.log(store);
+//   console.log(button.innerText);
   
-        div_3.append(div_4);
-          div_4.append(input);
-          div_4.append(label_1);
-          div_4.append(label_2);
-};
+//         div_3.append(div_4);
+//           div_4.append(input);
+//           div_4.append(label_1);
+//           div_4.append(label_2);
+// };
 
 
 
