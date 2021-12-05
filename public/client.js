48addee209720bd3dedfcef2bfd38989700dc586
Glitch (lace-heliotrope-microceratops) <none>
2021-12-05 14:29:38 +0000

    🦃🌒 Checkpoint ./views/index.ejs:38817841/390 ./public/client.js:38817841/3813

--- public/client.js
+++ public/client.js
@@ -29,14 +29,6 @@ fetch("/getUsersData", {})
   });
 
 
-//indexページ Menusデータを呼び出し
-// fetch("/getMenusData", {})
-//   .then(res => res.json())
-//   .then(response => {
-//     response.forEach(row => {
-//       appendNewMenuRadio(row.id, row.store, row.menu, row.price);
-//     });
-//   });
 
 fetch("/getMenusData", {})
   .then(res => res.json())
@@ -44,18 +36,7 @@ fetch("/getMenusData", {})
     response.forEach(row => {
       appendMenuAccordionHeader(row.id, row.store, row.menu, row.price);
     });
-    // response.forEach(row => {
-    //   appendMenuAccordionBody(row.id, row.store, row.menu, row.price);
-    // }); 
   });
-
-// fetch("/getMenusData", {})
-//   .then(res => res.json())
-//   .then(response => {
-//     response.forEach(row => {
-//       appendNewMenuAccordionBody(row.id, row.store, row.menu, row.price);
-//     });
-//   });
 
 
 //indexページ Usersデータ反映 ラジオボタン a helper function that creates a list item for a given user
@@ -75,87 +56,6 @@ const appendUserRadio = (id, user) => {
   div.append(input);
   div.append(label);
 }
-
-
-//indexページ Menusデータ反映 ラジオボタン a helper function that creates a list item for a given user
-// const appendMenuRadio = (id, store, menu, price) => {
-//   // console.log(id, user);
-//   const parent = document.getElementById("menusArea");
-//   const div = document.createElement("div");
-//     div.className = "form-check mb-4";
-//   const input = document.createElement("input");
-//     input.className = "form-check-input";
-//     input.type = "checkbox";
-//     input.name = "flexRadioDefault"
-//   const label_store = document.createElement("label");
-//     label_store.className = "form-check-label";
-//     label_store.innerText = store;
-//   const label_menu = document.createElement("label");
-//     label_menu.className = "form-check-label";
-//     label_menu.innerText = menu;
-//   const label_price = document.createElement("label");
-//     label_price.className = "form-check-label";
-//     label_price.innerText = price;
-//   parent.appendChild(div);
-//   div.append(input);
-//   div.append(label_store);
-//   div.append(label_menu);
-//   div.append(label_price);
-// }
-
-
-// 大元　indexページ Menusデータ反映 アコーディオン　ヘッダー
-// const appendMenuAccordionHeader = (id, store, menu, price) => {
-//   const parent = document.getElementById("menusArea");
-//   const div_1 = document.createElement("div");
-//     div_1.className = "accordion-item";
-//   const h2 = document.createElement("h2");
-//     h2.className = "accordion-header";
-//     h2.id = `heading_${id}`;
-//   const button = document.createElement("button");
-//     button.className = "accordion-button collapsed";
-//     button.type = "button";
-//     button.setAttribute("data-bs-toggle", "collapse");
-//     button.setAttribute("data-bs-target", `#collapse_${id}`);
-//     button.setAttribute("aria-expanded", "false");
-//     button.setAttribute("aria-controls", `collapse_${id}`);
-//     button.innerText = store;
-//   const div_2 = document.createElement("div");
-//     div_2.id = `collapse_${id}`;
-//     div_2.className = "accordion-collapse collapse";
-//     div_2.setAttribute("aria-labelledby", `heading_${id}`);
-//   // const body = appendNewMenuAccordionBody(id, store, menu, price);
-//   const div_3 = document.createElement("div");
-//     div_3.className = `accordion-body ${store}`;
-//   const div_4 = document.createElement("div");
-//     div_4.className = "form-check";
-//   const input = document.createElement("input");
-//     input.className = "form-check-input";
-//     input.type = "checkbox";
-//     input.value = "";
-//     input.id = "flexCheckDefault";
-//   const label_1 = document.createElement("label");
-//     label_1.className = "form-check-label";
-//     label_1.for = "flexCheckDefault";
-//     label_1.innerText = menu;
-//   const label_2 = document.createElement("label");
-//     label_2.className = "form-check-label px-3";
-//     label_2.for = "flexCheckChecked";
-//     label_2.innerText = price;
-  
-//   console.log(store);
-//   console.log(button.innerText);
-  
-//   parent.appendChild(div_1);
-//     div_1.append(h2);
-//       h2.append(button);
-//     div_1.append(div_2);
-//       div_2.append(div_3);
-//         div_3.append(div_4);
-//           div_4.append(input);
-//           div_4.append(label_1);
-//           div_4.append(label_2);
-// };
 
 
 // 試し　　indexページ Menusデータ反映 アコーディオン　ヘッダー
